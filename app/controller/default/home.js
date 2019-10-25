18ed506e08c0c4c9c7a819fc26b9a130e31e9d7f
wangpeng <email@qq.com>
2019-10-25 23:32:54 +0800

    add getArticleList api

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -4,10 +4,21 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    // 获取用户表的数据
-    const result = await this.app.mysql.get('blog_content', {});
-    console.log(result);
-    this.ctx.body = result;
+    this.ctx.body = 'lengband-blog-service index';
+  }
+  async getArticleList() {
+    let sql = 'SELECT article.id as id,' +
+      'article.title as title,' +
+      'article.introduce as introduce,' +
+      'article.addTime as addTime,' +
+      // "FROM_UNIXTIME(article.addTime, '%Y-%m-%d %H:%i:%s') as addTime," +
+      'article.view_count as view_count ,' +
+      'type.typeName as typeName ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.id'
+    const results = await this.app.mysql.query(sql)
+    this.ctx.body = {
+      data: results
+    }
   }
 }
 
