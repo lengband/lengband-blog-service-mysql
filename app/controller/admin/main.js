85d112db9acb1359dc8c9bfb2797e243dce5a4a2
lengband <lengband@163.com>
2020-01-02 23:01:34 +0800

    v1 done

--- app/controller/admin/main.js
+++ app/controller/admin/main.js
@@ -21,6 +21,72 @@ class MainController extends Controller {
     }
   }
 
+  //后台文章分类信息
+  async getTypeInfo() {
+    const resType = await this.app.mysql.select('type')
+    this.ctx.body = { data: resType }
+  }
+
+  //添加文章
+  async addArticle() {
+    let tmpArticle = this.ctx.request.body
+    // tmpArticle.
+    const result = await this.app.mysql.insert('article', tmpArticle)
+    const insertSuccess = result.affectedRows === 1
+    const insertId = result.insertId
+    this.ctx.body = {
+      isScuccess: insertSuccess,
+      insertId: insertId
+    }
+  }
+
+  //修改文章
+  async updateArticle() {
+    let tmpArticle = this.ctx.request.body
+    const result = await this.app.mysql.update('article', tmpArticle);
+    const updateSuccess = result.affectedRows === 1;
+    this.ctx.body = {
+      isScuccess: updateSuccess
+    }
+  }
+
+  //获得文章列表
+  async getArticleList() {
+    let sql = 'SELECT article.id as id,' +
+      'article.title as title,' +
+      'article.introduce as introduce,' +
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime," +
+      'type.typeName as typeName ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+      'ORDER BY article.id DESC '
+    const resList = await this.app.mysql.query(sql)
+    this.ctx.body = { list: resList }
+  }
+
+  //根据文章ID得到文章详情，用于修改文章
+  async getArticleById() {
+    let id = this.ctx.params.id
+    let sql = 'SELECT article.id as id,' +
+      'article.title as title,' +
+      'article.introduce as introduce,' +
+      'article.article_content as article_content,' +
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime," +
+      'article.view_count as view_count ,' +
+      'type.typeName as typeName ,' +
+      'type.id as typeId ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+      'WHERE article.id=' + id
+    const result = await this.app.mysql.query(sql)
+    this.ctx.body = { data: result }
+  }
+
+  //删除文章
+  async delArticle() {
+    let id = this.ctx.params.id
+    const res = await this.app.mysql.delete('article', { 'id': id })
+    this.ctx.body = { data: res }
+  }
+
   async index() {
     //首页的文章列表数据
     this.ctx.body = 'hi api'
